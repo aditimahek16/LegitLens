39eaa7862c8e52fbe3f12b61acfe938602bec4b4
Esita Budhia <72864242+aditimahek16@users.noreply.github.com>
2024-05-03 13:55:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -322,6 +322,7 @@ with right.form(key='try_it_out'):
         st.write('Here are all the feature weights that contributed to the decision.')
         st.write('\n\n'.join(map(lambda feature: f'The feature `{feature[0]}` has a weight of **{feature[1][1]}**. Multiplied by its value gives **{feature[1][0]}**.', items)))
     except Exception as error:
+      print("hi!")
       print("An exception occurred:", error)
       advice = st.write('*I don\'t think your URL worked. Please check your spelling or try another.*')
 
